384409f16bfe8c3bd435f5b79462b1aac214a8af
han <chos9715@naver.com>
2023-05-19 23:11:10 +0900

    feat: Security Config 생성

---
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.cos.photogramstart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    BCryptPasswordEncoder encode(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "comment/**").authenticated() // 해당 주소로 들어가면 인증 필요
+                .anyRequest().permitAll() // 그외 요청은 허용
+                .and()
+                .formLogin()// 접근 권한이 없을 때 로그인 창으로 애동
+                .loginPage("/auth/signin")
+                .defaultSuccessUrl("/"); // 정상 로그인시 이동
+        return http.build();
+    }
+}
