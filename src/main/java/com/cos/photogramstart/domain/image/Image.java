c57b3cd63d1496de798ed0ebb7bbb7f7903a2560
han <chos9715@naver.com>
2023-05-24 21:28:13 +0900

    feat: Image 업로드 기능 추가

---
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -0,0 +1,37 @@
+package com.cos.photogramstart.domain.image;
+
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String caption;
+    private String postImageUrl;
+
+    @JoinColumn(name = "userId")
+    @ManyToOne
+    private User user;
+
+    // 좋아요
+    // 댓글
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+}
---
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -0,0 +1,43 @@
+package com.cos.photogramstart.service;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.repository.ImageRepository;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    @Value("${file.path}")
+    private String uploadFolder;
+
+    @Transactional
+    public void upload(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails){
+        UUID uuid = UUID.randomUUID();
+        String imageFileName = uuid + "_" + imageUploadDto.getFile().getOriginalFilename();
+
+        Path imageFilePath = Paths.get(uploadFolder + imageFileName);
+
+        try{
+            Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        Image image = imageUploadDto.toEntity(imageFileName, principalDetails.getUser());
+        imageRepository.save(image);
+
+    }
+}
