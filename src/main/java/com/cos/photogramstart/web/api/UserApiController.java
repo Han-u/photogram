1b0f2ea7867efc9dfe4fcb4fed2572f5ef81017c
han <chos9715@naver.com>
2023-05-30 13:48:53 +0900

    feat: 유저 사진 변경

--- src/main/java/com/cos/photogramstart/web/api/UserApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/UserApiController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -57,4 +58,12 @@ public class UserApiController {
             return new CMRespDto<>(1, "회원수정완료", userEntity);
         }
     }
+
+    @PutMapping("/api/user/{principalId}/profileImageUrl")
+    public ResponseEntity<?> profileImageUrlUpdate(@PathVariable int principalId, MultipartFile profileImageFile, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        User userEntity = userService.updateProfile(principalId, profileImageFile);
+        principalDetails.setUser(userEntity);
+        return new ResponseEntity<>(new CMRespDto<>(1, "프로필 사진 변경 성공", null), HttpStatus.OK);
+    }
+
 }
--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -83,7 +83,12 @@ function getSubscribeModalItem(u) {
 
 
 // (3) 유저 프로파일 사진 변경 (완)
-function profileImageUpload() {
+function profileImageUpload(pageUserId, principalId) {
+	if(pageUserId !== principalId){
+		alert("잘못된 접근입니다.");
+		return;
+	}
+
 	$("#userProfileImageInput").click();
 
 	$("#userProfileImageInput").on("change", (e) => {
@@ -94,12 +99,32 @@ function profileImageUpload() {
 			return;
 		}
 
+		// 서버에 이미지 전송
+		let profileImageForm = $("#userProfileImageForm")[0];
+
+		// FormData 객체 이용하면 form 태그의 필드와 그 값을 나타내는 key/value 쌍을 담을 수 있음
+		let formData = new FormData(profileImageForm);
+
+		$.ajax({
+			type: "put",
+			url: `/api/user/${principalId}/profileImageUrl`,
+			data: formData,
+			contentType: false, // 필수: x-www-form-urlencoded로 파싱되는 것을 방지
+			processData: false, // 필수: contentType을 false로 줬을 때 QueryString 자도 설정됨. 해제
+			enctype: "multipart/form-data",
+			dataType: "json"
+		}).done(res => {
+			let reader = new FileReader();
+			reader.onload = (e) => {
+				$("#userProfileImage").attr("src", e.target.result);
+			}
+			reader.readAsDataURL(f); // 이 코드 실행시 reader.onload 실행됨.
+		}).fail(error => {
+			console.log("오류", error);
+		})
+
 		// 사진 전송 성공시 이미지 변경
-		let reader = new FileReader();
-		reader.onload = (e) => {
-			$("#userProfileImage").attr("src", e.target.result);
-		}
-		reader.readAsDataURL(f); // 이 코드 실행시 reader.onload 실행됨.
+
 	});
 }
 
