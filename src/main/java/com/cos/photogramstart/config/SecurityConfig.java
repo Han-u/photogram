ce172fc4cb7875958118c28c4d9aa7dae0bca80b
han <chos9715@naver.com>
2023-06-04 02:01:48 +0900

    feat: OAuth2 페이스북 로그인

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.cos.photogramstart.config;
 
+import com.cos.photogramstart.config.oauth.OAuth2DetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@RequiredArgsConstructor
+@EnableWebSecurity // 시큐리티 활성화
 @Configuration
 public class SecurityConfig {
+
+    private final OAuth2DetailsService oAuth2DetailsService;
 
     @Bean
     BCryptPasswordEncoder encode(){
@@ -24,7 +31,11 @@ public class SecurityConfig {
                 .formLogin()// 접근 권한이 없을 때 로그인 창으로 애동
                 .loginPage("/auth/signin") // GET
                 .loginProcessingUrl("/auth/signin") // POST
-                .defaultSuccessUrl("/"); // 정상 로그인시 이동
+                .defaultSuccessUrl("/") // 정상 로그인시 이동
+                .and()
+                .oauth2Login() // 일반 로그인도 하고 oauth2 로그인도 할 것이다
+                .userInfoEndpoint() //oauth2 로그인하면 최종응답을 회원정보로 바로 받아 --> 코드 X 액세스토큰 X
+                .userService(oAuth2DetailsService); // 받을
         return http.build();
     }
 }
--- src/main/java/com/cos/photogramstart/config/auth/PrincipalDetails.java
+++ src/main/java/com/cos/photogramstart/config/auth/PrincipalDetails.java
@@ -4,19 +4,27 @@ import com.cos.photogramstart.domain.user.User;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 @Data
-public class PrincipalDetails implements UserDetails {
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private static final long serialVersionUID = 1L;
 
     private User user;
+    private  Map<String, Object> attributes;
 
     public PrincipalDetails(User user){
         this.user = user;
+    }
+
+    public PrincipalDetails(User user, Map<String, Object> attributes){
+        this.user = user;
+
     }
 
     // 권한이 한 개가 아닐 수 있음
@@ -57,4 +65,14 @@ public class PrincipalDetails implements UserDetails {
         return true;
     }
 
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes; // {id:232323, name: hanu, email:dfa@naer.com}
+    }
+
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
 }
